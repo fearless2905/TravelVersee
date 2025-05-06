e61e35296a4bf384df4b27f0a94abe08755c84fc
Alif Bayu Ammarizky <alifbayuku@gmail.com>
2025-05-06 20:40:42 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
