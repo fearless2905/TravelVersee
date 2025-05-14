08738d41b190873d163944ca7d4bde0c96b8c037
Johnvickyrockk <vickyefendy100@gmail.com>
2025-05-14 15:52:42 +0700

    perbaikan header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,6 +24,25 @@ const Header = () => {
   return (
     <div>
       <div>
+        {/* Teks Heading dan Subheading */}
+        <h1 className="heading mb-4" data-aos="fade-up">
+          Jelajahi Banyuwangi dengan Virtual Reality
+        </h1>
+        <p className="sub-heading mb-5" data-aos="fade-up" data-aos-delay="100">
+          Jelajahi tempat eksotis Banyuwangi kapan saja, di mana saja!
+        </p>
+
+        {/* Tombol Aksi */}
+        <p className="pt-4 explore-btn" data-aos="fade-up" data-aos-delay="100">
+          <a
+            href="travelverse-dashboard.html"
+            className="btn uppercase btn-outline-light d-sm-inline d-block py-3"
+          >
+            Jelajahi Sekarang
+          </a>
+        </p>
+      </div>
+      <div>
         <img src={IjenImage} className="bg-image" alt="Background" />
       </div>
       <div className="brand">Travel Verse</div>
