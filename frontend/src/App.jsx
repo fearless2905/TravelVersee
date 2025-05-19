6da80814c4c3e04744d5d9279718709925831035
restuwintama <alditorestu00@gmail.com>
2025-05-19 18:01:38 +0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import Chatbot from "./components/Chatbot";
-=======
 import Dashboard from "./pages/Dasboard";
->>>>>>> 97a27bc8d5d9bb716fe31c047d708a32d1e6fcf0
 
 function App() {
   return (
@@ -12,13 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-<<<<<<< HEAD
       <Chatbot />
-=======
       <Routes>
         <Route path="dashboard" element={<Dashboard />} />
       </Routes>
->>>>>>> 97a27bc8d5d9bb716fe31c047d708a32d1e6fcf0
     </Router>
   );
 }
