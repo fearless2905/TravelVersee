b75158f747517464b3db75e43c7af88b88358a7a
restuwintama <alditorestu00@gmail.com>
2025-05-29 18:51:13 +0700

    ganti ke deepseek lagi karena gpt 3.5 turbo berbayar

--- backend/server/server.js
+++ backend/server/server.js
@@ -16,7 +16,7 @@ app.post("/api/chat", async (req, res) => {
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-        model: "openai/gpt-3.5-turbo", // Ganti dengan model yang sesuai keinginanmu yang penting model tersebut gratis(tidak berbayar)
+        model: "deepseek/deepseek-prover-v2:free", // Ganti dengan model yang sesuai keinginanmu yang penting model tersebut gratis(tidak berbayar)
         messages: messages,
       },
       {
