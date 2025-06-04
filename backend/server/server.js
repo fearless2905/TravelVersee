c5e3cd190c176bcb7326daa676748824d40d0322
restuwintama <alditorestu00@gmail.com>
2025-06-04 21:07:08 +0700

    hehe

--- backend/server/server.js
+++ backend/server/server.js
@@ -16,7 +16,7 @@ app.post("/api/chat", async (req, res) => {
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-        model: "deepseek/deepseek-prover-v2:free", // Ganti dengan model yang sesuai keinginanmu yang penting model tersebut gratis(tidak berbayar)
+        model: "deepseek/deepseek-chat:free", // Ganti dengan model yang sesuai keinginanmu yang penting model tersebut gratis(tidak berbayar)
         messages: messages,
       },
       {
